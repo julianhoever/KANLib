db219b0dde425c612d595c03e26b397c96128e59
Julian Hoever <julianhoever@posteo.de>
2025-09-08 12:59:01 +0200

    more flexible plotting
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -49,10 +49,10 @@ def _(Callable, TensorDataset, partial, torch):
         return TensorDataset(samples, targets)
 
     def func(inputs: torch.Tensor) -> torch.Tensor:
-        x, y = torch.unbind(inputs)
-        return torch.exp(torch.sin(x * torch.pi) + y**2)
+        x, y, z = torch.unbind(inputs)
+        return x * torch.exp(x**2 * torch.exp(z**2))
 
-    create_ds = partial(function_dataset, function=func, var_ranges=[(-1, 1), (-1, 1)])
+    create_ds = partial(function_dataset, function=func, var_ranges=[(-1, 1), (-1, 1), (-1, 1)])
     ds_train = create_ds(num_samples=2000)
     ds_val = create_ds(num_samples=400)
     return ds_train, ds_val
@@ -63,78 +63,59 @@ def _(Linear, ds_train, ds_val, partial, torch, train):
     linear = partial(
         Linear,
         spline_order=3,
-        grid_size=3,
+        grid_size=5,
         grid_range=(-1, 1),
         use_base_branch=True,
         use_spline_weight=True,
     )
 
     model = torch.nn.Sequential(
-        linear(in_features=2, out_features=1),
-        linear(in_features=1, out_features=1),
+        linear(in_features=3, out_features=3),
+        linear(in_features=3, out_features=1),
     )
     print("Params:", sum(p.numel() for p in model.parameters()))
 
-    def run_training() -> None:
-        train(
-            model=model,
-            ds_train=ds_train,
-            ds_val=ds_val,
-            epochs=100,
-            batch_size=32,
-            loss_fn=torch.nn.MSELoss(),
-            optimizer_cls=torch.optim.Adam,
-            optimizer_kwargs=dict(lr=1e-3),
-            load_best=False,
-            device=torch.device("cpu")
-        )
-        model.eval()
-
-    run_training()
-    return model, run_training
+    train(
+        model=model,
+        ds_train=ds_train,
+        ds_val=ds_val,
+        epochs=400,
+        batch_size=32,
+        loss_fn=torch.nn.MSELoss(),
+        optimizer_cls=torch.optim.Adam,
+        optimizer_kwargs=dict(lr=1e-3),
+        load_best=False,
+        device=torch.device("cpu")
+    )
+    _ = model.eval()
+    return (model,)
 
 
 @app.cell
 def _(linear_spline_index, model, plot_spline, plt):
-    def plot() -> None:
-        fig, axs = plt.subplots(nrows=2, ncols=2, tight_layout=True)
-        plot_spline(
-            layer=model[0],
-            spline_index=linear_spline_index(model[0], input_idx=0, output_idx=0),
-            show_grid=True,
-            ax=axs[0, 0]
-        )
-        plot_spline(
-            layer=model[0],
-            spline_index=linear_spline_index(model[0], input_idx=1, output_idx=0),
-            show_grid=True,
-            ax=axs[0, 1]
-        )
-        plot_spline(
-            layer=model[1],
-            spline_index=linear_spline_index(model[1], input_idx=0, output_idx=0),
-            show_grid=True,
-            ax=axs[1, 0]
-        )
-        return fig
+    fig, axs = plt.subplots(
+        nrows=len(model), 
+        ncols=max(l.in_features * l.out_features for l in model),
+        figsize=(10, 5),
+        sharey=True,
+        tight_layout=True
+    )
 
-    plot()
-    return (plot,)
+    for layer_idx in range(len(model)):
+        for out_idx in range(model[layer_idx].out_features):
+            for in_idx in range(model[layer_idx].in_features):
+                spline_idx = linear_spline_index(model[layer_idx], in_idx, out_idx)
+                plot_spline(
+                    layer=model[layer_idx],
+                    spline_index=spline_idx,
+                    show_grid=True,
+                    ax=axs[layer_idx, spline_idx]
+                )
 
+    for ax in axs.flatten():
+        ax.set_box_aspect(1)
 
-@app.cell
-def _(model, plot, run_training):
-    model[0].refine_grid(5)
-    run_training()
-    plot()
-    return
-
-
-@app.cell
-def _(model, plot, run_training):
-    model[0].refine_grid(10)
-    run_training()
-    plot()
+    fig
     return
 
 

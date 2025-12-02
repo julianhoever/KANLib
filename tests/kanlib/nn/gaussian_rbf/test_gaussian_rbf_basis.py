87a3892da274d683263c6b9e65324ebf76612dbf
Julian Hoever <julianhoever@posteo.de>
2025-12-02 15:02:39 +0100

    autoformat code
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- tests/kanlib/nn/gaussian_rbf/test_gaussian_rbf_basis.py
+++ tests/kanlib/nn/gaussian_rbf/test_gaussian_rbf_basis.py
@@ -37,18 +37,15 @@ def test_grid_oversample_is_equal(rbf: GaussianRbfBasis) -> None:
     ],
     ids=["single", "batch", "batch_with_channels"],
 )
-def test_forward_returns_correct_shape(
-    rbf: GaussianRbfBasis, x: torch.Tensor
-) -> None:
+def test_forward_returns_correct_shape(rbf: GaussianRbfBasis, x: torch.Tensor) -> None:
     assert rbf(x).shape == (*x.shape, rbf.num_basis_functions)
+
 
 @pytest.mark.parametrize("input_shape", [(1,), (3,), (2, 3), (2, 3, 1), (2, 3, 4, 5)])
 def test_forward_works_on_every_input_for_single_feature(
     input_shape: tuple[int, ...],
 ) -> None:
-    basis = GaussianRbfBasis(
-        grid_size=10, spline_range=torch.tensor([[-1.0, 1.0]])
-    )
+    basis = GaussianRbfBasis(grid_size=10, spline_range=torch.tensor([[-1.0, 1.0]]))
     inputs = torch.empty(*input_shape)
     outputs = basis(inputs)
     assert outputs.shape == (*inputs.shape, basis.num_basis_functions)

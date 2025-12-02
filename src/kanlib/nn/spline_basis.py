de15afbb6fd5a5a08bc17bbc6c299c636addb464
Julian Hoever <julianhoever@posteo.de>
2025-12-02 15:02:15 +0100

    transform spline_range to a property to make it more flexible
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/kanlib/nn/spline_basis.py
+++ src/kanlib/nn/spline_basis.py
@@ -16,15 +16,19 @@ class SplineBasis(torch.nn.Module, ABC):
             raise ValueError("`spline_range` must be of shape `(num_features, 2)`")
 
         self.grid_size = grid_size
-        self.spline_range = spline_range
+        self._initial_spline_range = spline_range
 
         self.grid: torch.Tensor
         self.register_buffer("grid", initialize_grid())
 
-        if self.grid.ndim != 2 or self.grid.shape[0] != self.spline_range.shape[0]:
+        if self.grid.ndim != 2 or self.grid.shape[0] != spline_range.shape[0]:
             raise ValueError(
                 "`grid` must be of shape `(num_features, num_grid_points)`"
             )
+
+    @property
+    def spline_range(self) -> torch.Tensor:
+        return self._initial_spline_range
 
     @property
     def num_features(self) -> int:

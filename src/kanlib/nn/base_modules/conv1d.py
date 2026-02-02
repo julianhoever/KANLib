6bc12e9f44308a76cbd4d31ade161537d44d57e2
Julian Hoever <julianhoever@posteo.de>
2026-02-02 12:26:18 +0100

    continue to work on conv1d layer implementation
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- src/kanlib/nn/base_modules/conv1d.py
+++ src/kanlib/nn/base_modules/conv1d.py
@@ -2,6 +2,7 @@ from functools import partial
 from typing import Literal
 
 import torch
+from torch.nn.functional import conv1d, silu
 from torch.nn.init import normal_ as init_normal
 from torch.nn.init import ones_ as init_ones
 from torch.nn.init import xavier_uniform_ as init_xavier_uniform
@@ -54,14 +55,50 @@ class Conv1d(KANBaseLayer):
             basis_factory=basis_factory,
             spline_input_norm=None,
         )
-        assert in_channels % groups == 0
-        assert out_channels % groups == 0
 
-    def residual_forward(self, x: torch.Tensor) -> torch.Tensor: ...
+        if groups <= 0:
+            raise ValueError("Number of groups must be a positive integer.")
+        if in_channels % groups != 0:
+            raise ValueError("`in_channels` must be divisible by `groups`.")
+        if out_channels % groups != 0:
+            raise ValueError("`out_channels` must be divisible by `groups`.")
+        if padding == "same" and stride != 1:
+            raise ValueError("Same padding is not supported for stride != 1.")
 
-    def spline_forward(self, x: torch.Tensor) -> torch.Tensor: ...
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
 
-def _validate_conv_params(in_channels: int, out_channels: int, groups: int, padding: int | PaddingStr) -> None:
-    if groups <= 0:
-        raise ValueError("Number of groups must be a positive integer")
-    if 
+    def residual_forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert self.weight_residual is not None
+        return conv1d(
+            input=silu(x),
+            weight=self.weight_residual,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            groups=self.groups,
+        )
+
+    def spline_forward(self, x: torch.Tensor) -> torch.Tensor:
+        basis: torch.Tensor = self.basis(x)
+
+        basis = _flatten_input_channels_and_basis(basis)
+        coeff = _flatten_input_channels_and_basis(self.weighted_coefficients)
+
+        return conv1d(
+            input=basis,
+            weight=coeff,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            groups=self.groups * self.basis.num_basis_functions,  # TODO: WRONG!!!
+        )
+
+
+def _flatten_input_channels_and_basis(x: torch.Tensor) -> torch.Tensor:
+    return torch.movedim(x, source=-1, destination=-2).flatten(start_dim=-3, end_dim=-2)
